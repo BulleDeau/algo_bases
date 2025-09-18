73750c4c26e0e18f807e5b50d79349a130b258ce
Muriel Bullot <murielbullot@hotmail.fr>
2025-09-18 15:52:11 +0200

    trouver_maximum_sets

--- ex02_boucles_listes_dictionaire/src/exercices.py
+++ ex02_boucles_listes_dictionaire/src/exercices.py
@@ -263,7 +263,25 @@ def table_multiplication_sets(n: int) -> set[int]:
 
 def trouver_maximum_sets(nums: set[int]) -> int:
     # TODO: Implémentez une fonction pour trouver et retourner le nombre maximum d'un set.
-    raise NotImplementedError
+    max = None
+    taille = len(nums)
+    count = 0
+    while count != taille:
+        count = 0
+        # passer en revu toutes les valeurs de nums
+        for i in nums:
+            if max is None:
+                max = i
+            if i > max:
+                max = i
+                count = 0
+            else:
+                count += 1
+        # si "i" n'est jamais plus grand que max pendant la totalité de la boucle alors
+        #       - count n'est jamais remis à 0
+        #       - count est incrémenté à chaque fois et vaut taille à la fin de la boucle
+        #  donc max contient le nombre le plus grand
+    return max
 
 
 def compter_negatifs_sets(nums: set[int]) -> int:
